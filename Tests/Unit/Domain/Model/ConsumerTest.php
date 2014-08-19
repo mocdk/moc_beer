ee656db501c421be2852af47b6a41f8a3e8313e6
Jan-Erik Revsbech <janerik@moc.net>
2014-08-19 18:23:21 +0200

    [FEATURE] Finish test og consumer for Opgave 2

--- Tests/Unit/Domain/Model/ConsumerTest.php
+++ Tests/Unit/Domain/Model/ConsumerTest.php
@@ -3,6 +3,7 @@
 
 class Tx_MocBeer_Domain_Model_ConsumerTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
 
+
 	/**
 	 * @test
 	 */
@@ -10,6 +11,27 @@ class Tx_MocBeer_Domain_Model_ConsumerTest extends Tx_Extbase_Tests_Unit_BaseTes
 		$beerMock = $this->getMock('Tx_MocBeer_Domain_Model_Beer');
 		$consumer = new Tx_MocBeer_Domain_Model_Consumer();
 		$beerMock->expects($this->once())->method('open');
+
+		$inventoryServiceMock = $this->getMock('Tx_MocBeer_Domain_Service_InventoryService', array(), array(), 'tempMock');
+		$consumer->injectInventoryService($inventoryServiceMock);
+
+		$consumer->drinkBeer($beerMock);
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function testThatInventoryServiceIsCalledWhenConsumerDrinksBeer() {
+
+		$beerMock = $this->getAccessibleMock('Tx_MocBeer_Domain_Model_Beer');
+		$beerMock->_set('name', 'Test beer');
+		$consumer = $this->objectManager->get('Tx_MocBeer_Domain_Model_Consumer');
+
+		$inventoryServiceMock = $this->getMock('Tx_MocBeer_Domain_Service_InventoryService', array(), array(), 'tempMock');
+		$consumer->injectInventoryService($inventoryServiceMock);
+		$inventoryServiceMock->expects($this->once())->method('removeBeerFromInventory')->will($this->returnValue(TRUE));
+
 		$consumer->drinkBeer($beerMock);
 	}
 
