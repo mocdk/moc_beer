0963594508244736ac9f0fcca7d0397994a1bde0
Jan-Erik Revsbech <janerik@moc.net>
2014-08-19 16:09:38 +0200

    [FEATURE] Added simple tests for Beer

---
+++ Tests/Unit/Domain/Model/BeerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class Tx_MocBeer_Domain_Model_BeerTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * This method is called before each test is executed
+	 */
+	public function setup() {
+	}
+
+	/**
+	 * This method is called after each test is executed
+	 */
+	public function tearDown() {
+	}
+
+	/**
+	 * @test
+	 * @group small
+	 */
+	public function testGetNameUsingSetter() {
+		$beer = new Tx_MocBeer_Domain_Model_Beer();
+		$beer->setName('Lager');
+		$this->assertEquals('Lager', $beer->getName());
+	}
+
+	/**
+	 * @test
+	 */
+	public function testGetNameUsingProxy() {
+		$proxyName = $this->buildAccessibleProxy('Tx_MocBeer_Domain_Model_Beer');
+		$beer = new $proxyName();
+		$beer->_set('name', 'Lager');
+		$this->assertEquals('Lager', $beer->getName());
+	}
+	
+}
